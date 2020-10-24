b44bfb43b2a1e5167040dcb61736274600c6f982
DevanshiPatel18 <61454611+DevanshiPatel18@users.noreply.github.com>
2020-10-24 17:30:56 +0530

    Update BubbleSort_PHP.php
    added comments to understand the code

--- Bubble_Sort/BubbleSort_PHP.php
+++ Bubble_Sort/BubbleSort_PHP.php
@@ -1,7 +1,8 @@
 <?php
+//implementation of the function
 function bubble_Sort($my_array )
 {
-	do
+	do // repest till array not sorted
 	{
 		$swapped = false;
 		for( $i = 0, $c = count( $my_array ) - 1; $i < $c; $i++ )
@@ -14,13 +15,13 @@ function bubble_Sort($my_array )
 			}
 		}
 	}
-	while( $swapped );
+	while( $swapped ); // if swapped value then true, so continue else break
 return $my_array;
 }
-echo "Enter values of array in a comma seprated form - like- 1,8,4,9\n";
-$test_array = explode(',', readline()); 
+echo "Enter values of array in a comma seprated form - like- 1,8,4,9\n"; //get set of values from user
+$test_array = explode(',', readline()); //split the string into an array
 echo "Original Array :\n";
-echo implode(',',$test_array );
+echo implode(',',$test_array ); //print original array
 echo "\nSorted Array\n:";
-echo implode(', ',bubble_Sort($test_array)). PHP_EOL;
+echo implode(', ',bubble_Sort($test_array)). PHP_EOL; //print sorted array
 ?>
